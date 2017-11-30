98c6ba8d8b5d49cbf46cec0b09b332290ebce66b
Macc <maccavalli@gmail.com>
2017-11-30 13:36:03 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/*global $*/
+/*global $ajax*/
+
+$(() => {
+    
+    $ajax({
+        url: "https://newsapi.org/v2/sources",
+        method: "GET",
+        data: {
+            category: "business",
+            language: "en",
+            country: "us",
+            apiKey: "APIKEY"
+        }
+        
+    })
+})
