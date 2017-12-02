851a8a25234d47ec358979abb654c58a6a522621
Macc <maccavalli@gmail.com>
2017-12-02 18:11:01 +0000

    add for loop

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*global $*/
+/*global $ APIKEY*/
 
 $(() => {
     
@@ -10,7 +10,27 @@ $(() => {
             language: "en",
             country: "us",
             apiKey: "APIKEY"
+        },
+        success: function(data) {
+            for (var i = 0; i < data.sources.length; i++) {
+                var source = document.createElement("OPTION");
+                source.setAttribute("value", data.sources[i].id);
+                source.innerHTML = data.sources[i].name;
+                document.getElementById("selection").appendChild(source);
+            }
         }
         
-    })
-})
+    });
+});
+
+$("#source").submit(function(event) {
+    event.preventDefault();
+    $.ajax({
+        url: "https://newsapi.org/v2/top-headlines",
+        method: "GET",
+        data: {
+            sources: (document.getElementById("selection").value),
+            apiKey: APIKEY
+        },
+    });
+});
